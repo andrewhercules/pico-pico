eb9549d6ddfee9b0ecc83fe06291eaf44d9c1fc3
Andrew Hercules <andrewhercules@gmail.com>
2014-10-26 00:26:02 +0100

    Updated stylesheet.css with new styles of sign-in form button

--- public/scripts/interface.js
+++ public/scripts/interface.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
 
 	// // Use the code below to hide the sign-in-box after page loads
+	
 	// $('#sign-in-box').hide();
 
-	$.getJSON('/api', function(peepData){
 
-		$('#app=a').text(peepData[1]["name"])
-	
-		$('#username').text(peepData[0]["username"])
-	
-	});
+	// $.getJSON('/api', function(peepData){
 
+	// 	$('#app=a').text(peepData[1]["name"])
 	
+	// 	$('#username').text(peepData[0]["username"])
+	
+	// });
+
+$('#sign-in-button').on('click', function() {
+  $('#sign-in-box').toggle();
+});
 
 
 
