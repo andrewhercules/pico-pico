515c3e6e1623d131f0447c05f9090a0bee35045b
Andrew Hercules <andrewhercules@gmail.com>
2014-10-25 16:52:38 +0100

    Updated api.rb controller to create an array of all peeps and user details, which is then converted in JSON format

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -1,5 +1,37 @@
-get '/api' do
-  content_type :json
-  @peeps = Peep.all.reverse
-	@peeps.to_json
+get '/api/peeps' do
+ 	
+	@peeps = Peep.all 	
+
+	@api_data = Array.new
+
+	@peeps.each do |peep|
+	
+		ind_peep = Hash.new
+			ind_peep["username"] = peep.user.username
+			ind_peep["name"] = peep.user.name
+			ind_peep["message"] = peep.message
+			ind_peep["date"] = peep.date
+
+		@api_data << ind_peep
+	
+	end
+
+	@api_data.to_json
+
 end
+
+
+
+# Results in /api page showing <DataMapper::Query>
+
+#  	@api_hash = {
+# 		"username" => Peep.user.username,
+# 		"name" => Peep.user.name,
+# 		"date" => Peep.date,
+# 		"message" => Peep.message,		
+#  	}
+
+
+
+
+# 	@api_hash.to_json
--- app/controllers/peeps.rb
+++ app/controllers/peeps.rb
@@ -1,5 +1,4 @@
 post '/peeps' do
-	content_type :json
 	Peep.create(:message => params["message"], :user_id => session["user_id"])
 	redirect to ('/')
 end
