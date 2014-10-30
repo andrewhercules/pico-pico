c215d4ca9a7bd6e174127923e106917fd508091d
Andrew Hercules <andrewhercules@gmail.com>
2014-10-30 18:57:05 +0000

    Updated CSS stylesheet and created new erb files to utilize Sinatra partials

--- public/scripts/interface.js
+++ public/scripts/interface.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-	// // Use the code below to hide the sign-in-box after page loads
-	
 	$('#sign-in-box').hide();
 	$('#forgot-password-box').hide();
 
@@ -12,37 +10,30 @@ $(document).ready(function() {
 	  $('#sign-up-box').toggle(); 
 	})
 
+	$('#sign-up-button').on('click', function() {
+	  $('#sign-up-box').toggle();
+	  $('#sign-in-box').toggle(); 
+	})
+
 	$('#forgot-password-button').on('click', function() {
 	  $('#forgot-password-box').toggle();
 	})
 
 
 	$.getJSON('/api', function(peepData){
-		var source   = $("#peep-template").html();
+		var source   = $('#peep-template').html();
 		var template = Handlebars.compile(source);
 		var peepList = $.each(peepData, function(count, listOfAllPeeps) {})
 
-		$.each(peepList, function(count, individualPeep) {
-			$('#list-of-peeps').append(template(individualPeep))
-			return count < 9;
-
-		})
-
-	$(document).on("click", "#remove", function(){
-   $(this).parent('li').remove();
+			$.each(peepList, function(count, individualPeep) {
+				$('#list-of-peeps').append(template(individualPeep))
+				return count < 9;
+			})
 	});
 
-// ('#list-of-peeps').append(template(individualPeep));
 
-
-		// $.each(peepData, function(count, individualPeep) {
-		// 	console.log(individualPeep);
-		// })
-
-		// $('#app=a').text(peepData[1]["name"])
-	
-		// $('#username').text(peepData[0]["username"])
-	
+	$(document).on('click', '.remove-peep', function(){
+   $(this).parent('li').remove();
 	});
 
 
